1acbc3db5c2ff3f88da7810a322c87689ca74057
PauloMonte <>
2021-04-22 01:16:42 -0300

    More comments added

--- Assets/ClockSystem/ClockDisplayFormatter.cs
+++ Assets/ClockSystem/ClockDisplayFormatter.cs
@@ -9,12 +9,19 @@ public struct ClockDisplayFormatter
     /// Formats TimeSpan in time/clock display
     /// </summary>
     /// <param name="timeSpane"></param>
-    /// <returns>hh:mm:ss</returns>
+    /// <returns>time display string in hh:mm:ss format</returns>
     public string FormatClockDisplay(TimeSpan timeSpane)
     {        
         return FormatClockDisplay(timeSpane.Hours, timeSpane.Minutes, timeSpane.Seconds);
     }
 
+    /// <summary>
+    /// Formats a time/clock display by given hour, minutes and seconds
+    /// </summary>
+    /// <param name="hours"></param>
+    /// <param name="minutes"></param>
+    /// <param name="seconds"></param>
+    /// <returns>time display string in hh:mm:ss format</returns>
     string FormatClockDisplay(int hours, int minutes, int seconds)
     {
         string labelHour = FormatClockPiece(hours);
@@ -23,12 +30,23 @@ public struct ClockDisplayFormatter
         return $"{labelHour}:{labelMinutes}:{labelSeconds}";
     }
 
+    /// <summary>
+    /// Formats a seconds display
+    /// </summary>
+    /// <param name="seconds"></param>
+    /// <returns>seconds string in ss format</returns>
     string FormatClockDisplaySecondsOnly(int seconds)
     {        
         string labelSeconds = FormatClockPiece(seconds);
         return $"{labelSeconds}";
     }
 
+    /// <summary>
+    /// Formats a seconds display
+    /// </summary>
+    /// <param name="minutes"></param>
+    /// <param name="seconds"></param>
+    /// <returns>time display in mm:ss format</returns>
     string FormatClockDisplayMinutesAndSecondsOnly(int minutes, int seconds)
     {
         string labelMinutes = FormatClockPiece(minutes);
@@ -36,6 +54,11 @@ public struct ClockDisplayFormatter
         return $"{labelMinutes}:{labelSeconds}";
     }
 
+    /// <summary>
+    /// Format a timer piece by the given value
+    /// </summary>
+    /// <param name="timePiece"></param>
+    /// <returns>piece of timer in nn format, like nn:nn:nn</returns>
     string FormatClockPiece(int timePiece)
     {
         return timePiece < 10 ? $"0{timePiece}" : timePiece.ToString();
